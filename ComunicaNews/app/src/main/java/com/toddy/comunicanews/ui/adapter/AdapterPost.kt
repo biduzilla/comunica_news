7fccf6e2f03041b437f4c0707395fa35cb31b386
biduzilla <rlyowlly19@gmail.com>
2023-04-28 09:22:26 -0300

    adapter post

--- ComunicaNews/app/src/main/java/com/toddy/comunicanews/ui/adapter/AdapterPost.kt
+++ ComunicaNews/app/src/main/java/com/toddy/comunicanews/ui/adapter/AdapterPost.kt
@@ -1,4 +1,58 @@
 package com.toddy.comunicanews.ui.adapter
 
-class AdapterPost {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.toddy.comunicanews.databinding.ItemPostBinding
+import com.toddy.comunicanews.models.Post
+
+class AdapterPost(
+    posts: List<Post> = emptyList(),
+    var onClick: (post: Post) -> Unit = {}
+) : RecyclerView.Adapter<AdapterPost.ViewHolder>() {
+
+    private val posts = posts.toMutableList()
+
+    inner class ViewHolder(private val binding: ItemPostBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        private lateinit var post: Post
+
+        init {
+            itemView.setOnClickListener {
+                if (::post.isInitialized) {
+                    onClick(post)
+                }
+            }
+        }
+
+        fun vincula(post: Post) {
+            this.post = post
+
+            with(binding) {
+                tvTitulo.text = post.titulo
+                tvDesc.text = post.descricao
+                tvQtdLikes.text = post.likes.toString()
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemPostBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val post = posts[position]
+        holder.vincula(post)
+    }
+
+    override fun getItemCount(): Int = posts.size
+
+    fun atualiza(posts: List<Post>) {
+        this.posts.clear()
+        this.posts.addAll(posts.reversed())
+        notifyDataSetChanged()
+    }
 }
